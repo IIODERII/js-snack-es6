80824a607458daa1d333d5ad0ec4cc253b7007ec
IIODERII <oderrisi@gmail.com>
2023-10-25 15:13:30 +0200

    added script for snack 3

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 (function oder2Snacks(){
     snack1() ;
     snack2() ;
+    snack3() ;
 
     function snack1(){
         const tavoloVip = [ 'Brad Pitt',
@@ -80,5 +81,72 @@
         const studUp70and120 = students.filter((value) => value.grades > 70 && value.id > 120);
         console.log('La lista degli studenti che hanno un totale di voti superiore a 70 e id superiore a 120');
         console.log(studUp70and120);
+    }
+
+    function snack3(){
+        const biciclette = [
+            {
+                nome: 'Atala',
+                peso: ''
+            },
+            {
+                nome: 'Basso',
+                peso: ''
+            },
+            {
+                nome: 'Bianchi',
+                peso: ''
+            },
+            {
+                nome: 'Bottecchia',
+                peso: ''
+            },
+            {
+                nome: 'Brinke ',
+                peso: ''
+            },
+            {
+                nome: 'Canellini',
+                peso: ''
+            },
+            {
+                nome: 'Lombardo',
+                peso: ''
+            },
+            {
+                nome: 'Pinarello',
+                peso: ''
+            },
+            {
+                nome: 'Scapin',
+                peso: ''
+            },
+            {
+                nome: 'Olympia',
+                peso: ''
+            },
+        ]
+
+        biciclette.forEach((value) => value.peso = getRndInteger(2,23));
+        console.log('Ecco tutte le nostre biciclette: ');
+        console.log(biciclette);
+
+        const snack3Container = document.getElementById('snack-3');
+        const lightBikeMsg = document.createElement('div');
+        lightBikeMsg.className = 'fs-3 py-5 px-3';
+
+        let lightBike = biciclette[0];
+
+        biciclette.forEach((value) => lightBike = (lightBike.peso > value.peso) ? value : lightBike);
+
+        lightBikeMsg.innerHTML = `
+            Nome della bicicletta: ${lightBike.nome}.<br>
+            Peso della bicicletta: ${lightBike.peso} kg.
+        `;
+        snack3Container.append(lightBikeMsg);
+    }
+
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
     }
 }());
